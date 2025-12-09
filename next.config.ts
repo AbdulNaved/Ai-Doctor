338b50c2b51fab61fb99be6c29b838ce04b92364
AbdulNaved <140192855+AbdulNaved@users.noreply.github.com>
2025-12-09 05:06:43 -0500

    updated

--- next.config.ts
+++ next.config.ts
@@ -7,6 +7,23 @@ const nextConfig = {
       { hostname: "images.unsplash.com" },
     ],
   },
+  eslint: {
+    // Prevent build from failing because of lint errors in generated files (Prisma)
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
+
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   images: {
+//     remotePatterns: [
+//       { hostname: "localhost" },
+//       { hostname: "randomuser.me" },
+//       { hostname: "images.unsplash.com" },
+//     ],
+//   },
+// };
+
+// export default nextConfig;
